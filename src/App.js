3a71c4c11b33a3ae558019d7a24e543096cf0796
developer-build <developerbuild62@gmail.com>
2022-06-29 01:13:48 +0600

    set up

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello Developer Build</h1>
-      <p>sfjsadikfh</p>
-      <div>adsfdopgiufdohj</div>
+      <h1>developer build project</h1>
+      <p>Hello Developer Build</p>
     </div>
   );
 }
