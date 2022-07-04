ccc4cf11d28bf99dcd675bbdc0c340c0fa89c80d
Muntasir Ahmed <muntasirahmed@Muntasirs-MacBook-Air.local>
2022-07-04 13:00:58 +0600

    navber fully done

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
   
