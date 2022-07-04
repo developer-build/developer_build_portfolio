0c7087d461075111153223fdd1bc4e8ee5ed2857
SunjidaAkter <sanjidabintesayid7@gmail.com>
2022-07-04 22:40:54 +0600

    blog section completed

--- src/Components/Home/BlogSection/BlogSection.js
+++ src/Components/Home/BlogSection/BlogSection.js
@@ -4,15 +4,15 @@ import './BlogSection.css'
 
 const BlogSection = () => {
     return (
-        <div className='custom_container'>
+        <div className='custom_container px-10'>
             <p className='text-[18px] font-bold text-primary text-center mb-[0.5rem]'>Latest Posts</p>
-            <p className='text-accent font-bold text-center text-[42px] uppercase'>OUR LATEST STORIES </p>
-            <p className='text-accent font-bold text-center text-[42px] uppercase '>AND ARTICLES</p>
+            <p className='text-[#2c3957] font-bold text-center text-[42px] uppercase'>OUR LATEST STORIES </p>
+            <p className='text-[#2c3957] font-bold text-center text-[42px] uppercase '>AND ARTICLES</p>
             <div className='flex justify-center items-center'>
                 <div className='hr'>
                 </div>
             </div>
-            <div className='relative grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2 lg:pb-[100px] md:pb-0 pb-[0px] gap-4 '>
+            <div className='relative grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2 lg:pb-[100px] md:pb-0 pb-[0px] gap-8 '>
                 {BlogData.map((blog, i) =>
                     <div className='blog'>
                         <div className=''>
@@ -23,10 +23,10 @@ const BlogSection = () => {
                         </div>
                         <div className='blog-container px-[40px] pt-[34px] pb-[42px]'>
                             <div className='flex justify-between items-center'>
-                                <p className='text-[15px] font-[600] text-[#888888]'>By Admins</p>
-                                <p className='text-[15px] font-[600] text-[#888888]'>{blog.comments} Comments</p>
+                                <p className='text-[15px] font-[600] text-[#888888]'><span className='text-primary'>icon</span> By Admins</p>
+                                <p className='text-[15px] font-[600] text-[#888888]'><span className='text-primary'>icon</span> {blog.comments} Comments</p>
                             </div>
-                            <p className='my-5 text-[24px] text-accent font-bold'>{blog.title}</p>
+                            <p className='title my-5 text-[24px] text-[#2c3957] font-bold'>{blog.title}</p>
                             <p className='text-[15px] text-[#666666] '>{blog.des}</p>
                         </div>
                     </div>
