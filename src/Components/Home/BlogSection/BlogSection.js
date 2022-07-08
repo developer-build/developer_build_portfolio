719842b52a5315acce0cf7bf0c9c97db86d145e4
developer-sahadat <mdridoysikder862@gmail.com>
2022-07-08 21:05:48 +0600

    error slove

--- src/Components/Home/BlogSection/BlogSection.js
+++ src/Components/Home/BlogSection/BlogSection.js
@@ -35,26 +35,6 @@ const BlogSection = () => {
               </div>
             </div>
 
-            <div className='relative grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2 lg:pb-[100px] md:pb-0 pb-[0px] gap-8 '>
-                {BlogData.map((blog, i) =>
-                    <div key={blog?.id} className='blog'>
-                        <div className=''>
-                            <img className='w-full z-0' src={blog.img} alt="Blog related image" />
-                            <div className='mt-[-30px]'>
-                                <p className=' ml-[40px] text-[15px]  z-10 text-white bg-primary px-[19px] py-[9px] inline'>{blog.date}</p>
-                            </div>
-                        </div>
-                        <div className='blog-container px-[40px] pt-[34px] pb-[42px]'>
-                            <div className='flex justify-between items-center'>
-                                <p className='text-[15px] font-[600] text-[#888888]'><span className='text-primary'>icon</span> By Admins</p>
-                                <p className='text-[15px] font-[600] text-[#888888]'><span className='text-primary'>icon</span> {blog.comments} Comments</p>
-                            </div>
-                            <p className='title my-5 text-[24px] text-[#2c3957] font-bold'>{blog.title}</p>
-                            <p className='text-[15px] text-[#666666] '>{blog.des}</p>
-                        </div>
-                    </div>
-                )}
-
             <div className="blog-container px-[40px] pt-[34px] pb-[42px]">
               <div className="flex justify-between items-center">
                 <p className="text-[15px] font-[600] text-[#888888]">
@@ -73,7 +53,6 @@ const BlogSection = () => {
                 {blog.title}
               </p>
               <p className="text-[15px] text-[#666666] ">{blog.des}</p>
-
             </div>
           </article>
         ))}
